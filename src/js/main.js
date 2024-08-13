0601063277a7f3a091806b2516f7d1ebcd6803d9
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2024-08-13 11:17:05 +0300

    add ui components and classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
-import $ from'./lib/lib';
+import $ from './lib/lib';
 
-$('button').on('click', function() {
-  $('div').eq(2).toggleClass('active');
+$('#first').on('click', () => {
+  $('div').eq(1).fadeOut(800);
 });
 
-$('div').click(function() {
-  console.log($(this).index());
-})
+$('[data-count="second"]').on('click', () => {
+  $('div').eq(2).fadeOut(800);
+});
 
-// console.log($('.some').closest('.findme'));
-// console.log($('div').eq(2).find('.some'));
-$('button').fadeIn(2000);
+$('button').eq(2).on('click', () => {
+  $('.w-500').fadeOut(800);
+});
